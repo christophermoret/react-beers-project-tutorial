de07c2380fc6daaa8be286fdf386818fd1828b00
Marco Tribuzio <marco.tribuz@gmail.com>
2021-02-12 11:57:16 +0100

    Added Container

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import ContactUs from "./components/pages/contactUs/contactUs";
 import AboutUs from "./components/pages/aboutUs/aboutUs";
 import TableListBeers from "./components/pages/listBeers/tableListBeers";
 import Navbar from "./components/layout/navbar";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
@@ -18,39 +20,28 @@ function App() {
       <Router>
         <div>
           <Navbar/>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about-us">About</Link>
-              </li>
-              <li>
-                <Link to="/contact-us">Users</Link>
-              </li>
-              <li>
-                <Link to="/table-list-beers">Table List</Link>
-              </li>
-            </ul>
-          </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
+          <CssBaseline />
+          <Container>
+
+            {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about-us">
-              <AboutUs />
-            </Route>
-            <Route path="/contact-us">
-              <ContactUs />
-            </Route>
-            <Route path="/table-list-beers">
-              <TableListBeers />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route path="/about-us">
+                <AboutUs />
+              </Route>
+              <Route path="/contact-us">
+                <ContactUs />
+              </Route>
+              <Route path="/table-list-beers">
+                <TableListBeers />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+
+          </Container>
         </div>
       </Router>
     </div>
